1c523ff78acb8984358094bbd575acf87f8ae216
lenagend <57205046+lenagend@users.noreply.github.com>
2023-12-05 07:55:43 +0900

    도커파일

--- src/components/RecommendedNumbersCard.js
+++ src/components/RecommendedNumbersCard.js
@@ -9,8 +9,6 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red} from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import {Box, CircularProgress, Divider, Fab, Stack} from "@mui/material";
@@ -118,9 +116,6 @@ export default function RecommendedNumbersCard() {
                 </Box>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                </IconButton>
                 <IconButton aria-label="share" onClick={() => {htmlToImageDownloader('recommendedNumbersCard')}}>
                     <Download />
                 </IconButton>
@@ -159,6 +154,11 @@ export default function RecommendedNumbersCard() {
                                         {recommend.reason && (
                                             <Typography variant="subtitle2">
                                                 ({recommend.reason})
+                                            </Typography>
+                                        )}
+                                        {recommend.frequencyInfo && (
+                                            <Typography variant="subtitle2">
+                                                ({recommend.frequencyInfo})
                                             </Typography>
                                         )}
                                     </Stack>
--- src/components/WinningNumbersCard.js
+++ src/components/WinningNumbersCard.js
@@ -9,8 +9,6 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import {red} from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import {Box, CircularProgress, Pagination, Stack} from "@mui/material";
@@ -90,9 +88,6 @@ export default function WinningNumbersCard() {
                 </Box>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                </IconButton>
                 <IconButton aria-label="share" onClick={() => htmlToImageDownloader('winningNumbersCard')}>
                     <Download />
                 </IconButton>
